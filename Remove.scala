9da1dae03e73b514ef50890de2c64f06f26f7799
Xinran <yuxinran8011@gmail.com>
2019-08-27 15:10:41 -0500

    Update Remove.scala

--- Remove.scala
+++ Remove.scala
@@ -1,6 +1,10 @@
-//'Rextester' class is the entry point for your code.
-//Don't declare a package.
-//scala 2.11.7
+//Given an array nums, write a function to move all 0's 
+//to the end of it while maintaining the relative order of the non-zero elements.
+
+//Example:
+
+//Input: [0,1,0,3,12]
+//Output: [1,3,12,0,0]
 
 object Remove extends App {
     println("Hello, World!")
